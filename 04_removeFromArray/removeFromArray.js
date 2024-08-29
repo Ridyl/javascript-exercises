f9393a2c4f66392527724b7e3ee197243a1edade
Ridyl <joey.dalatri214@gmail.com>
2024-08-29 13:09:39 -0400

    04 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+
+    num.forEach((element) => {
+        for (let i = array.length; i >= 0; i--) {    //reversed loop to iterate from last index to first.
+            if(array[i] === element) {
+                array.splice(i, 1);
+            }
+        }
+    })
+
+    return array;
 };
 
 // Do not edit below this line
