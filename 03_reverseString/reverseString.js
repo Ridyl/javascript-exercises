68ec7bcfc40bf30c8d36fd27c819430cd3f1af7c
Ridyl <joey.dalatri214@gmail.com>
2024-08-29 12:27:21 -0400

    03 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let nameArray = [];
 
+    for(let i = 0; i < string.length; i++) {
+        nameArray.push(string[i]);
+    }
+
+    let reverseName = nameArray.reverse().join('');
+    return reverseName;
+    
 };
 
 // Do not edit below this line
