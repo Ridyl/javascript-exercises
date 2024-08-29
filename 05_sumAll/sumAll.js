d9008fc979adf74c0d8201ab1c71066ba908c84b
Ridyl <joey.dalatri214@gmail.com>
2024-08-29 13:38:29 -0400

    05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
 
+    if (startNum < 0 || endNum < 0) {
+        return `ERROR`;
+    } else if (Number.isInteger(startNum) == false || Number.isInteger(endNum) == false) {
+        return `ERROR`;
+    }
+
+    if (startNum < endNum) {
+        for(i = startNum; i <= endNum; i++) {
+            sum += i;
+        }
+    } else if (startNum > endNum) {
+        for(i = startNum; i >= endNum; i--) {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
