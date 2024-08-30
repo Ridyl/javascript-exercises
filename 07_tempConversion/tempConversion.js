35e8c73cd2c1424f7cea3f94892bdb98ee63231c
Ridyl <joey.dalatri214@gmail.com>
2024-08-30 04:14:51 -0400

    07 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  // C = (F - 32) / 1.8 -- fahrenheit to celsius conversion
+  let cel = (far - 32) / 1.8;
+  return Math.round(cel * 10)/10; //could use toFixed() but would have to convert back to float from string.
+                                  
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  // F = C * 1.8 + 32 -- celsius to fahrenheit conversion
+  let far = cel * 1.8 + 32;
+  return Math.round(far * 10)/10;
+
 };
 
 // Do not edit below this line
