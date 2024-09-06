49792bae770783f8b3a7513020b460c380ceb632
Ridyl <joey.dalatri214@gmail.com>
2024-09-05 22:20:44 -0400

    08 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const sum = array.reduce((a, b) => a + b, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(mult) {
+  const multiply = mult.reduce((a, b) => a * b, 1);
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let multiply = a;
+	for(let i = 1; i < b; i++) {
+    multiply *= a;
+  }
+  return multiply;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+	for(let i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
