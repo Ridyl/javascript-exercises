bb6458a1889e2a7f59ef1d4bfff4cb587231a0da
Ridyl <joey.dalatri214@gmail.com>
2024-09-07 12:43:02 -0400

    Not as condensed as the solution but still does the same task. And finishes in the exact same time as the solution answer does.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = [];
+    let largest = 0;
+    let index = 0;
 
+    people.forEach(element => {
+        if('yearOfDeath' in element && 'yearOfBirth' in element) {
+            oldest.push(element.yearOfDeath - element.yearOfBirth);
+        } else {
+            element.yearOfDeath = new Date().getFullYear();
+            oldest.push(element.yearOfDeath - element.yearOfBirth);
+        }
+    });
+
+    for(let i = 0; i < oldest.length; i++) {
+        if (oldest[i] > largest) {
+            largest = oldest[i];
+            index = i;
+        }
+    }
+
+    return people[index];
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
