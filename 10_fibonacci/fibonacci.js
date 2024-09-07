bd220f3553cdcfdcbf1726ed3a745df3be196d2c
Ridyl <joey.dalatri214@gmail.com>
2024-09-06 22:47:24 -0400

    Thought this would take a bit longer. Has similar structure to solution and doesn't have any 'Out There' solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+//N = N-1 + N-2 where N > 1
 
+const fibonacci = function(sequenceNum) {
+    let finArray = [1, 1];
+
+    if (sequenceNum < 0) {
+        return "OOPS";
+    } else if (sequenceNum == 0) {
+        return 0;
+    } else if (typeof sequenceNum === 'string') {
+        sequenceNum = Number(sequenceNum);
+    }
+
+    for (let i = 1; i < sequenceNum; i++) {
+        let finNum = finArray[finArray.length - 1] + finArray[finArray.length - 2];
+        finArray.push(finNum)
+    }
+
+    return finArray[sequenceNum - 1];
 };
 
 // Do not edit below this line
