c5cd2a27c941a574f757e03ad3d7daf1f0335709
Ridyl <joey.dalatri214@gmail.com>
2024-09-06 23:02:12 -0400

    books was an array of objects... took me too long to see the []

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    //new array for just the object title values
+    let bookTitle = [];
 
+    //forEach to iterate over the objects within the books array
+    books.forEach(element => {
+        bookTitle.push(element.title);
+    });
+
+    return bookTitle;
 };
 
 // Do not edit below this line
